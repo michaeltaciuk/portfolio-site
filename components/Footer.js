d74c3bb0a3c55eb8d3ced4d5c711e3d58066dfa7
Michael Taciuk <taciuk73@gmail.com>
2022-09-21 19:32:50 -0400

    Header and Footer Changes

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,6 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <div className="foot">
-      <div>
         <Link href="https://github.com/michaeltaciuk" >
             <a className="foot-link">GitHub</a>
         </Link>
@@ -14,7 +13,6 @@ export default function Footer() {
         <Link href="https://twitter.com/michael_taciuk">
             <a className="foot-link">Twitter</a>
         </Link>
-      </div>
     </div>
   )
 }
